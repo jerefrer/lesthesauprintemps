5b8a65b34a603f34271bf23d26696b8996ab9163
Jérémy FRERE <frere.jeremy@gmail.com>
2016-04-23 00:00:42 +0200

    Met à jour la nav et le hero aux couleurs du festival

--- themes/agency/static/js/cbpAnimatedHeader.js
+++ themes/agency/static/js/cbpAnimatedHeader.js
@@ -13,13 +13,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 200;
+		changeHeaderOn = 20;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				scrollPage();
 			}
 		}, false );
 	}
